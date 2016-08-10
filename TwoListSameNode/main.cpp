1a3eff3a42484e395d6ce1cad3f48b88a364e0f2
陈宇峰 <yufengcode@gmail.com>
2016-08-10 17:37:18 +0800

    开始构建。

--- TwoListSameNode/main.cpp
+++ TwoListSameNode/main.cpp
@@ -8,8 +8,100 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+/**
+ *  找到两个链表的第一个公共节点
+ *
+ */
+
+using namespace std;
+
+typedef struct ListNode{
+
+    int element;
+    struct ListNode *next;
+}Node;
+
+void CreateList(Node **pHead);
+
+// 创建第二个链表，让其最后的指针指向第一个链表中的某个节点
+void CreateSecondList(Node **pHead, Node *pFirstList);
+
+void PrintSameNode(Node *pFirstList, Node *pSecondList);
+
+void PrintList(Node *pListHead);
+
+int main(int argc, const char * argv[])
+{
+    Node *pFirstList;
+    Node *pSecondList;
+
+    CreateList(&pFirstList);
+    PrintList(pFirstList);
+
+    CreateSecondList(&pSecondList, pFirstList);
+
+
+
     return 0;
 }
+
+void CreateList(Node **pHead)
+{
+    cout << "请输入节点值创建链表，输入<=0时结束" << endl;
+
+    *pHead = new Node;
+    memset(*pHead, 0, sizeof(Node));
+    (*pHead)->next = NULL;
+
+    Node *pListNode = new Node;
+    memset(pListNode, 0, sizeof(Node));
+    pListNode->next = NULL;
+
+    Node *pMove = *pHead;
+
+    cin >> pListNode->element;
+    while (pListNode->element > 0)
+    {
+        pMove->next = pListNode;
+        pMove = pMove->next;
+
+        pListNode = new Node;
+        memset(pListNode, 0, sizeof(Node));
+        pListNode->next = NULL;
+
+        cin >> pListNode->element;
+    }
+}
+
+void CreateSecondList(Node **pHead, Node *pFirstList)
+{
+
+}
+
+void PrintSameNode(Node *pFirstList, Node *pSecondList)
+{
+
+}
+
+void PrintList(Node *pListHead)
+{
+    Node *pMove = pListHead->next;
+
+    while (pMove != NULL)
+    {
+        cout << pMove->element << " ";
+        pMove = pMove->next;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
